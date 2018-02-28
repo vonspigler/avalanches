fc55f251d1e9a9ba009cf4dadbf7fea78167a8a7
vonspigler <stefano@spigler.net>
2018-02-28 14:20:35 +0100

    Updated avalanches.py -- looking for the correct tolerances.

--- avalanches.py
+++ avalanches.py
@@ -1,25 +1,25 @@
-########################################################################
-##                                                                    ##
-##      AVALANCHES                                                    ##
-##     ----------------------------------------------------------     ##
-##                                                                    ##
-##      I want to train a network (find a good minimum) and then      ##
-##      add a perturbing force (e.g.\ linear coupling with the        ##
-##      weights. Under this force, I minimize again, and then I       ##
-##      can study the distribution of avalanches.                     ##
-##                                                                    ##
-##      Q: Are there wiser choices for the perturbation?              ##
-##                                                                    ##
-##      Q: I don't expect the distribution to be power-law dist.,     ##
-##         can I still say something about the landscape?             ##
-##                                                                    ##
-########################################################################
-##                                                                    ##
-##      TODO:                                                         ##
-##                                                                    ##
-##      * Write the code...?                                          ##
-##                                                                    ##
-########################################################################
+################################################################################
+##                                                                            ##
+##      AVALANCHES                                                            ##
+##     ----------------------------------------------------------             ##
+##                                                                            ##
+##      I want to train a network (find a good minimum) and then              ##
+##      add a perturbing force (e.g.\ linear coupling with the                ##
+##      weights. Under this force, I minimize again, and then I               ##
+##      can study the distribution of avalanches.                             ##
+##                                                                            ##
+##      Q: Are there wiser choices for the perturbation?                      ##
+##                                                                            ##
+##      Q: I don't expect the distribution to be power-law dist.,             ##
+##         can I still say something about the landscape?                     ##
+##                                                                            ##
+################################################################################
+##                                                                            ##
+##      TODO:                                                                 ##
+##                                                                            ##
+##      * Write the code...?                                                  ##
+##                                                                            ##
+################################################################################
 
 
 import os
@@ -33,7 +33,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 
-# --  Models  -------------------------------------------------------- #
+# --  Models  ---------------------------------------------------------------- #
 
 
 class SimpleNet(torch.nn.Module):
@@ -65,19 +65,19 @@ class SimpleNet(torch.nn.Module):
         return x
 
 
-# --  Datasets  ------------------------------------------------------ #
+# --  Datasets  -------------------------------------------------------------- #
 
 
 # Fashion-MNIST dataset: 1 channel, 10 classes, 28x28 pixels
 # Normalized as MNIST -- I should probably change it
 trainset = list(datasets.FashionMNIST(
-	'../data/',
-	train = True,
-	download = True,
-	transform = transforms.Compose([
-		transforms.ToTensor(),
-		transforms.Normalize((0.1307,), (0.3081,))
-	])
+    '../data/',
+    train = True,
+    download = True,
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
 ))
 
 #testset = list(datasets.FashionMNIST('../data/', train = False, download = True, transform = transforms.Compose([ \
@@ -86,7 +86,7 @@ trainset = list(datasets.FashionMNIST(
 #])))
 
 
-# --  Other definitions  --------------------------------------------- #
+# --  Other definitions  ----------------------------------------------------- #
 
 
 class RandomSampler:
@@ -121,64 +121,82 @@ def load_batch(loader, cuda = False, only_one_epoch = False):
         if only_one_epoch: break  # exit the loop if only_one_epoch == True
 
 
-# --  Training function  --------------------------------------------- #
+# --  Training function  ----------------------------------------------------- #
 
 
-def train_and_save(model, trainset, lr, bs, minimization_time, file_state, file_losses, time_factor = None):
-    """
+def minimize_step(model, optimizer, data, target):
+    output = model(data)
+    loss = F.nll_loss(output, target, size_average = True)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss
+
+def compute_batch_loss_grad(model, optimizer, data, target):
+    output = model(data)
+    loss = F.nll_loss(output, target, size_average = False)
+    optimizer.zero_grad()
+    loss.backward()
+    grad = torch.cat([ p.grad.view(-1) for p in model.parameters() ])
+    return loss, grad
+
+def minimize(model, trainset, lr, bs, delta_tolerance, grad_tolerance, time_factor):
+    """Train the model on the dataset until abs(Loss(t) - Loss(t-1)) < delta_tolerance.
+
+     -- lr, bs are the learning rate and batch size.
+     -- time_factor is used to build a logarithmic time scale; this is used to
+        compute the TOTAL Loss at those times: this TOTAL Loss is used to
+        check for convergence (Delta TOTAL Loss < delta_tolerance).
     """
 
     if cuda.is_available(): model.cuda()
     model.train()  # not necessary in this simple model, but I keep it for the sake of generality
-    optimizer = optim.SGD(model.parameters(), lr = lr)	# learning rate
+    optimizer = optim.SGD(model.parameters(), lr = lr)    # learning rate
 
     trainloader = DataLoader(
-        trainset,								# dataset
-        batch_size = bs,						# batch size
-        pin_memory = cuda.is_available(),		# speed-up for gpu's
-        sampler = RandomSampler(len(trainset))	# no epochs
+        trainset,                                # dataset
+        batch_size = bs,                        # batch size
+        pin_memory = cuda.is_available(),        # speed-up for gpu's
+        sampler = RandomSampler(len(trainset))    # no epochs
     )
 
-    if time_factor == None: time_factor = minimization_time**(1.0/200)
+    prev_loss = np.inf
+    curr_loss = 0
+    curr_grad_norm = 0
+
     next_t = 1.0
     batch = 0
 
-    with open(file_losses, 'wb') as losses_dump:
-        for data, target in load_batch(trainloader, cuda = cuda.is_available()):
-            batch += 1
-            if batch > minimization_time:
-                break
+#    with open(file_losses, 'wb') as losses_dump:
+    for data, target in load_batch(trainloader, cuda = cuda.is_available()):
+        batch += 1
+        minimize_step(model, optimizer, data, target)
 
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.nll_loss(output, target, size_average = True)
-            loss.backward()
-            optimizer.step()
+        if batch > next_t:
+            next_t *= time_factor
+            avg_loss, avg_grad = 0, 0
+            total_trainloader = DataLoader(
+                trainset,
+                batch_size = 1024,  # I don't need small batches for this
+                pin_memory = cuda.is_available(),
+                sampler = RandomSampler(len(trainset))
+            )
 
-            if batch > next_t:
-                # I want to save the average loss on the total training set
-                avg_loss = 0
-                total_trainloader = DataLoader(
-                    trainset,
-                    batch_size = 1024,  # I don't need small batches for this
-                    pin_memory = cuda.is_available(),
-                    sampler = RandomSampler(len(trainset))
-                )
+            for data, target in load_batch(total_trainloader, cuda = cuda.is_available(), only_one_epoch = True):
+                d_avg_loss, d_avg_grad = compute_batch_loss_grad(model, optimizer, data, target)
+                avg_loss += d_avg_loss.data[0]
+                avg_grad += d_avg_grad
 
-                for data, target in load_batch(total_trainloader, cuda = cuda.is_available(), only_one_epoch = True):
-                    output = model(data)
-                    avg_loss += F.nll_loss(output, target, size_average = False).data[0]
+            curr_grad_norm = float(torch.norm(avg_grad/len(trainset)).data[0])
+            curr_loss = avg_loss/len(trainset)
+#            print("#", curr_loss, abs(curr_loss - prev_loss), curr_grad_norm)
+            if abs(curr_loss - prev_loss) < delta_tolerance and curr_grad_norm < grad_tolerance: break
+            prev_loss = curr_loss
 
-                pickle.dump(( batch, avg_loss/len(trainset) ), losses_dump)
-                next_t *= time_factor
-
-    state_dict = model.state_dict()  # == losses[-1]['state_dict']
-    torch.save(state_dict, file_state)
-
-    return state_dict
+    return curr_loss, curr_grad_norm
 
 
-# ==  MAIN  ========================================================== #
+# ==  MAIN  ================================================================== #
 
 
 # input_channels, output_classes, image_size (Fashion-MNIST = 28x28 -> size = 28)
@@ -186,10 +204,15 @@ network_parameters = (1, 10, 28)
 model = SimpleNet(*network_parameters)
 
 # number of perturbation steps
-num_steps = 100  # JUST A TEST ########################################
+num_steps = 10  # JUST A TEST ##################################################
 
-# minimizations stop when L(t) - L(t-1) < delta_tolerance:
-delta_tolerance = 1e-4  # JUST A TEST ##################################
+# minimizations stop when L(t) - L(t-1) < delta_tolerance and
+# |grad| < grad_tolerance:
+delta_tolerance = 1e-3  # JUST A TEST ##########################################
+grad_tolerance = 0.3  # JUST A TEST ############################################
+# time_factor is used to define logarithmic time intervals to compute TOTAL
+# loss and gradient, to check for convergence
+time_factor = 1.2
 
 # temperatures, LR and BS
 lr = 0.01
@@ -197,14 +220,24 @@ bs = 64
 temp = lr/bs
 
 
-# --  Prepare the system  -------------------------------------------- #
+# --  Prepare the system  ---------------------------------------------------- #
 
 
-init_loss = minimize(model, lr, bs, delta_tolerance = delta_tolerance)
-print(0, 0, init_loss)
+print("Training the system for the first time...")
+init_loss, init_grad_norm = minimize(model, trainset, lr, bs,
+    delta_tolerance = delta_tolerance,
+    grad_tolerance = grad_tolerance,
+    time_factor = time_factor
+)
+print(0, 0, init_loss, init_grad_norm, 0)
 prev_loss = init_loss
 
+print("Perturbation steps...")  # FOR THE MOMENT: NO PERTURBATION ##############
 for t in range(num_steps):
-	curr_loss = minimize(model, lr, bs, delta_tolerance = delta_tolerance)
-	print(t*lr, curr_loss, curr_loss - prev_loss)  # JUST A TEST #######
-	prev_loss = curr_loss
+    curr_loss, curr_grad_norm = minimize(model, trainset, lr, bs,
+        delta_tolerance = delta_tolerance,
+        grad_tolerance = grad_tolerance,
+        time_factor = time_factor
+    )
+    print(t*lr, curr_loss, abs(curr_loss - prev_loss), curr_grad_norm)
+    prev_loss = curr_loss
